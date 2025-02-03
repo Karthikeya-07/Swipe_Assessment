4755a07200999d32da0136e8027dc0cc5a4f567e
Karthik <akarthikeya463@gmail.com>
2025-02-03 21:17:58 +0530

    Initial Commit

---
+++ SwipeAssessment/SwipeAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeAssessmentApp.swift
+//  SwipeAssessment
+//
+//  Created by Akepati Karthikeya Reddy on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SwipeAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
